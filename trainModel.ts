beedc24ae3afe02f3bd6ce5578b9ad01c23d5127
Zyad Abdul-Nasser <zizo_007@live.com>
2024-04-20 19:45:55 +0200

    z

--- trainModel.ts
+++ trainModel.ts
@@ -11,12 +11,22 @@ async function trainModel(manager: any) {
   const addRequirement = await trainIntent(manager, 'add.requirement', '../data/add.requirement.txt', '../answers/add.requirement.txt')
   const sendTeam = await trainIntent(manager, 'send.team', '../data/send.team.txt', '../answers/send.team.txt')
   const listVendors = await trainIntent(manager, 'list.vendors', '../data/list.vendors.txt', '../answers/list.vendors.txt')
+  const strategy = await trainIntent(manager, 'strategy', '../data/strategy.txt', '../answers/strategy.txt')
+  const proposeVendors = await trainIntent(manager, 'propose.vendors', '../data/propose.vendors.txt', '../answers/propose.vendors.txt')
+  const chartTCO = await trainIntent(manager, 'chart.TCO', '../data/chart.TCO.txt', '../answers/chart.TCO.txt')
+  const generateRFP = await trainIntent(manager, 'generate.rfp', '../data/generate.rfp.txt', '../answers/generate.rfp.txt')
+  const portal = await trainIntent(manager, 'portal', '../data/portal.txt', '../answers/portal.txt')
   if (
     greetings &&
     listRequirements &&
     addRequirement &&
     sendTeam &&
-    listVendors
+    listVendors &&
+    strategy && 
+    proposeVendors && 
+    chartTCO && 
+    generateRFP &&
+    portal
   ) {
     await manager.train()
     await manager.save();
