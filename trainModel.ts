babffd7d40d866854472b08df645312b5b6c5063
anas12312 <anashesham000@gmail.com>
2024-04-20 19:48:51 +0200

    anas

--- trainModel.ts
+++ trainModel.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { write } from 'fs';
 import trainIntent from './utils/trainIntent';
 
 async function trainModel(manager: any) {
@@ -11,12 +11,20 @@ async function trainModel(manager: any) {
   const addRequirement = await trainIntent(manager, 'add.requirement', '../data/add.requirement.txt', '../answers/add.requirement.txt')
   const sendTeam = await trainIntent(manager, 'send.team', '../data/send.team.txt', '../answers/send.team.txt')
   const listVendors = await trainIntent(manager, 'list.vendors', '../data/list.vendors.txt', '../answers/list.vendors.txt')
+  const sourcingOptions = await trainIntent(manager, 'sourcing.options', '../data/sourcing.options.txt', '../answers/sourcing.options.txt')
+  const sendMessage = await trainIntent(manager, 'send.message', '../data/send.message.txt', '../answers/send.message.txt')
+  const writeThank = await trainIntent(manager, 'write.thank', '../data/write.thank.txt', '../answers/write.thank.txt')
+  const sendThank = await trainIntent(manager, 'send.thank', '../data/send.thank.txt', '../answers/send.thank.txt')
   if (
     greetings &&
     listRequirements &&
     addRequirement &&
     sendTeam &&
-    listVendors
+    listVendors &&
+    sourcingOptions &&
+    sendMessage &&
+    writeThank &&
+    sendThank
   ) {
     await manager.train()
     await manager.save();
